2644998fa35b1e32a1cae39f2508e1671b4c514b
SecretsDev <josegalabarc@gmail.com>
2026-01-16 13:13:24 -0600

    Nivel 2: menú responsive y sección habilidades con cards

--- js/main.js
+++ js/main.js
@@ -1,1 +1,8 @@
-console.log("Portafolio de Jose Galvez cargado correctamente 🚀");
+console.log("Portafolio cargado 🚀");
+
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+    navLinks.classList.toggle("active");
+});
